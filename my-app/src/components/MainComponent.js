f81d760c0b699ac7134328f9bbf5b99cc043367d
sunzid02 <sunzid02@gmail.com>
2020-07-20 02:08:50 +0600

    assignemnt 2

--- my-app/src/components/MainComponent.js
+++ my-app/src/components/MainComponent.js
@@ -8,6 +8,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
@@ -62,6 +63,7 @@ class Main extends Component {
                         <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
                         <Route exact path='/contactus' component={Contact} />} />
                         <Route path='/menu/:dishId' component={DishWithId} />
+                        <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
 
                         <Redirect to="/home" />
                     </Switch>
