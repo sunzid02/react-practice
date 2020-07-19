ab26fc1b2cfb3fc0b064f2936cb10260a8e38dcc
sunzid02 <sunzid02@gmail.com>
2020-07-20 00:22:36 +0600

    Single Page Applications Part 1

--- my-app/src/components/MainComponent.js
+++ my-app/src/components/MainComponent.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect, Router } from 'react-router-dom';
 
 import Menu from './MenuComponent';
 import DishDetail from "./DishDetailComponent";
-import { DISHES } from '../shared/dishes';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
+
+import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
+
 
 
 class Main extends Component {
@@ -17,10 +23,12 @@ class Main extends Component {
 
         this.state = {
             dishes: DISHES,
-            selectedDish: null
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
         };
-
     }
+
 
     onDishSelect(dishId) {
         this.setState({ selectedDish: dishId });
@@ -32,6 +40,9 @@ class Main extends Component {
         const HomePage = () => {
             return (
                 <Home
+                    dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                    promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                    leader={this.state.leaders.filter((leader) => leader.featured)[0]}
                 />
             );
         }
@@ -42,6 +53,8 @@ class Main extends Component {
                     <Switch>
                         <Route path='/home' component={HomePage} />
                         <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+                        <Route exact path='/contactus' component={Contact} />} />
+                        
                         <Redirect to="/home" />
                     </Switch>
                 <Footer/>
